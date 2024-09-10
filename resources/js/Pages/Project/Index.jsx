de81c9ce8c05e027bf4869e232bba181b15837d0
sirpolux <poluxreloaded@gmail.com>
2024-09-10 13:50:55 +0100

    update

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,5 +1,5 @@
 import Authenticated from "@/Layouts/AuthenticatedLayout"
-import { Head } from "@inertiajs/react"
+import { Head, Link } from "@inertiajs/react"
 
 export default function Index({auth,projects}){
     return (
@@ -13,7 +13,46 @@ export default function Index({auth,projects}){
                         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                             <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                                 <div className="p-6 text-gray-900">
-                                    <pre>{JSON.stringify(projects,undefined,2)}</pre>
+                                    <table className="w-full text-sm">
+                                        <thead className="text-xs text-gray-700 uppercase">
+                                            <tr className="text-nowrap bg-gray-500">
+                                                <th className="px-3 py-2">ID</th>
+                                                <th className="px-3 py-2">Image</th>
+                                                <th className="px-3 py-2">Name</th>
+                                                <th className="px-3 py-2">Status</th>
+                                                <th className="px-3 py-2">Create Date</th>
+                                                <th className="px-3 py-2">Due date</th>
+                                                <th className="px-3 py-2">Created by</th>
+                                                <th className="px-3 py-2">Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {projects.data.map((project)=>(
+                                                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                                    <th className="px-3 py-2">{project.id}</th>
+                                                    <td className="px-3 py-2">
+                                                        <img src={project.image_path} alt="" style={{width:60}} />
+                                                    </td>
+                                                    <td className="px-3 py-2">{project.name}</td>
+                                                    <td className="px-3 py-2">{project.status}</td>
+                                                    <td className="px-3 py-2">{project.created_at}</td>
+                                                    <td className="px-3 py-2">{project.due_date}</td>
+                                                    <td className="px-3 py-2">{project.createdBy.name}</td>
+                                                    <td className="px-3 py-2">
+                                                        <Link className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"  href={route('project.edit',project.id)}>
+                                                            Edit
+                                                        </Link>
+                                                        <Link className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"  href={route('project.destroy',project.id)}>
+                                                            Delete
+                                                        </Link>
+                                                    </td>
+                                                 
+
+                                                </tr> 
+                                            ))}
+                                          
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
