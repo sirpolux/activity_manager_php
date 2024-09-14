9cca94a238bea7a24de790ea3e723f1e021e7e19
sirpolux <poluxreloaded@gmail.com>
2024-09-14 22:21:01 +0100

    update

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    
+    public function assignedUser(){
+        return $this->belongsTo(User::class, "assigned_user_id");
+    }
 }
